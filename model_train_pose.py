f180c1bdbfefbed2455c6c32686d65dc5875b12e
Dawi Lu <dawi9840@gmail.com>
2021-08-27 14:00:49 +0800

    Update model_train_pose.py

--- model_train_pose.py
+++ model_train_pose.py
@@ -32,6 +32,10 @@ class CsvDataset:
             'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8', 'y9', 'y10', 'y11',
             'z1', 'z2', 'z3', 'z4', 'z5', 'z6', 'z7', 'z8', 'z9', 'z10', 'z11',
             'v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9', 'v10', 'v11',
+
+            'v12', 'v13', 'v14', 'v15', 'v16', 'v17', 'v18', 'v19', 'v20', 'v21',
+            'v22', 'v23', 'v24', 'v25', 'v26', 'v27', 'v28', 'v29', 'v30', 'v31',
+            'v32', 'v33'
         ]
 
         df2 = df2.drop(columns_removed, axis = 'columns')
@@ -183,28 +187,28 @@ def input_features():
     z32 = keras.Input(shape=(1,), name=coords[2][20])
     z33 = keras.Input(shape=(1,), name=coords[2][21])
 
-    v12 = keras.Input(shape=(1,), name=coords[3][0])
-    v13 = keras.Input(shape=(1,), name=coords[3][1])
-    v14 = keras.Input(shape=(1,), name=coords[3][2])
-    v15 = keras.Input(shape=(1,), name=coords[3][3])
-    v16 = keras.Input(shape=(1,), name=coords[3][4])
-    v17 = keras.Input(shape=(1,), name=coords[3][5]) 
-    v18 = keras.Input(shape=(1,), name=coords[3][6])
-    v19 = keras.Input(shape=(1,), name=coords[3][7])
-    v20 = keras.Input(shape=(1,), name=coords[3][8])
-    v21 = keras.Input(shape=(1,), name=coords[3][9])
-    v22 = keras.Input(shape=(1,), name=coords[3][10])
-    v23 = keras.Input(shape=(1,), name=coords[3][11])
-    v24 = keras.Input(shape=(1,), name=coords[3][12])
-    v25 = keras.Input(shape=(1,), name=coords[3][13])
-    v26 = keras.Input(shape=(1,), name=coords[3][14])
-    v27 = keras.Input(shape=(1,), name=coords[3][15])
-    v28 = keras.Input(shape=(1,), name=coords[3][16])
-    v29 = keras.Input(shape=(1,), name=coords[3][17])
-    v30 = keras.Input(shape=(1,), name=coords[3][18])
-    v31 = keras.Input(shape=(1,), name=coords[3][19])
-    v32 = keras.Input(shape=(1,), name=coords[3][20])
-    v33 = keras.Input(shape=(1,), name=coords[3][21])
+    # v12 = keras.Input(shape=(1,), name=coords[3][0])
+    # v13 = keras.Input(shape=(1,), name=coords[3][1])
+    # v14 = keras.Input(shape=(1,), name=coords[3][2])
+    # v15 = keras.Input(shape=(1,), name=coords[3][3])
+    # v16 = keras.Input(shape=(1,), name=coords[3][4])
+    # v17 = keras.Input(shape=(1,), name=coords[3][5]) 
+    # v18 = keras.Input(shape=(1,), name=coords[3][6])
+    # v19 = keras.Input(shape=(1,), name=coords[3][7])
+    # v20 = keras.Input(shape=(1,), name=coords[3][8])
+    # v21 = keras.Input(shape=(1,), name=coords[3][9])
+    # v22 = keras.Input(shape=(1,), name=coords[3][10])
+    # v23 = keras.Input(shape=(1,), name=coords[3][11])
+    # v24 = keras.Input(shape=(1,), name=coords[3][12])
+    # v25 = keras.Input(shape=(1,), name=coords[3][13])
+    # v26 = keras.Input(shape=(1,), name=coords[3][14])
+    # v27 = keras.Input(shape=(1,), name=coords[3][15])
+    # v28 = keras.Input(shape=(1,), name=coords[3][16])
+    # v29 = keras.Input(shape=(1,), name=coords[3][17])
+    # v30 = keras.Input(shape=(1,), name=coords[3][18])
+    # v31 = keras.Input(shape=(1,), name=coords[3][19])
+    # v32 = keras.Input(shape=(1,), name=coords[3][20])
+    # v33 = keras.Input(shape=(1,), name=coords[3][21])
 
     all_inputs = [
         x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, 
@@ -216,8 +220,8 @@ def input_features():
         z12, z13, z14, z15, z16, z17, z18, z19, z20, z21, z22, 
         z23, z24, z25, z26, z27, z28, z29, z30, z31, z32, z33,
 
-        v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, 
-        v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33,
+        # v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, 
+        # v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33,
     ]
 
     return all_inputs
@@ -226,115 +230,115 @@ def input_features():
 def specify_encoded_features(train_ds, all_inputs):
 
     encoded = EncodeFeatures()
-    coords = point()
+    coords_p = point()
 
     # point 12 ~ 33
     # Numerical features
-    x12 = encoded.numerical_feature(all_inputs[0], coords[0][0], train_ds)
-    x13 = encoded.numerical_feature(all_inputs[1], coords[0][1], train_ds)
-    x14 = encoded.numerical_feature(all_inputs[2], coords[0][2], train_ds)
-    x15 = encoded.numerical_feature(all_inputs[3], coords[0][3], train_ds)
-    x16 = encoded.numerical_feature(all_inputs[4], coords[0][4], train_ds)
-    x17 = encoded.numerical_feature(all_inputs[5], coords[0][5], train_ds)
-    x18 = encoded.numerical_feature(all_inputs[6], coords[0][6], train_ds)
-    x19 = encoded.numerical_feature(all_inputs[7], coords[0][7], train_ds)
-    x20 = encoded.numerical_feature(all_inputs[8], coords[0][8], train_ds)
-    x21 = encoded.numerical_feature(all_inputs[9], coords[0][9], train_ds)
-    x22 = encoded.numerical_feature(all_inputs[10], coords[0][10], train_ds)
-    x23 = encoded.numerical_feature(all_inputs[11], coords[0][11], train_ds)
-    x24 = encoded.numerical_feature(all_inputs[12], coords[0][12], train_ds)
-    x25 = encoded.numerical_feature(all_inputs[13], coords[0][13], train_ds)
-    x26 = encoded.numerical_feature(all_inputs[14], coords[0][14], train_ds)
-    x27 = encoded.numerical_feature(all_inputs[15], coords[0][15], train_ds)
-    x28 = encoded.numerical_feature(all_inputs[16], coords[0][16], train_ds)
-    x29 = encoded.numerical_feature(all_inputs[17], coords[0][17], train_ds)
-    x30 = encoded.numerical_feature(all_inputs[18], coords[0][18], train_ds)
-    x31 = encoded.numerical_feature(all_inputs[19], coords[0][19], train_ds)
-    x32 = encoded.numerical_feature(all_inputs[20], coords[0][20], train_ds)
-    x33 = encoded.numerical_feature(all_inputs[21], coords[0][21], train_ds)
+    x12 = encoded.numerical_feature(all_inputs[0], coords_p[0][0], train_ds)
+    x13 = encoded.numerical_feature(all_inputs[1], coords_p[0][1], train_ds)
+    x14 = encoded.numerical_feature(all_inputs[2], coords_p[0][2], train_ds)
+    x15 = encoded.numerical_feature(all_inputs[3], coords_p[0][3], train_ds)
+    x16 = encoded.numerical_feature(all_inputs[4], coords_p[0][4], train_ds)
+    x17 = encoded.numerical_feature(all_inputs[5], coords_p[0][5], train_ds)
+    x18 = encoded.numerical_feature(all_inputs[6], coords_p[0][6], train_ds)
+    x19 = encoded.numerical_feature(all_inputs[7], coords_p[0][7], train_ds)
+    x20 = encoded.numerical_feature(all_inputs[8], coords_p[0][8], train_ds)
+    x21 = encoded.numerical_feature(all_inputs[9], coords_p[0][9], train_ds)
+    x22 = encoded.numerical_feature(all_inputs[10], coords_p[0][10], train_ds)
+    x23 = encoded.numerical_feature(all_inputs[11], coords_p[0][11], train_ds)
+    x24 = encoded.numerical_feature(all_inputs[12], coords_p[0][12], train_ds)
+    x25 = encoded.numerical_feature(all_inputs[13], coords_p[0][13], train_ds)
+    x26 = encoded.numerical_feature(all_inputs[14], coords_p[0][14], train_ds)
+    x27 = encoded.numerical_feature(all_inputs[15], coords_p[0][15], train_ds)
+    x28 = encoded.numerical_feature(all_inputs[16], coords_p[0][16], train_ds)
+    x29 = encoded.numerical_feature(all_inputs[17], coords_p[0][17], train_ds)
+    x30 = encoded.numerical_feature(all_inputs[18], coords_p[0][18], train_ds)
+    x31 = encoded.numerical_feature(all_inputs[19], coords_p[0][19], train_ds)
+    x32 = encoded.numerical_feature(all_inputs[20], coords_p[0][20], train_ds)
+    x33 = encoded.numerical_feature(all_inputs[21], coords_p[0][21], train_ds)
 
-    y12 = encoded.numerical_feature(all_inputs[22], coords[1][0], train_ds)
-    y13 = encoded.numerical_feature(all_inputs[23], coords[1][1], train_ds)
-    y14 = encoded.numerical_feature(all_inputs[24], coords[1][2], train_ds)
-    y15 = encoded.numerical_feature(all_inputs[25], coords[1][3], train_ds)
-    y16 = encoded.numerical_feature(all_inputs[26], coords[1][4], train_ds)
-    y17 = encoded.numerical_feature(all_inputs[27], coords[1][5], train_ds)
-    y18 = encoded.numerical_feature(all_inputs[28], coords[1][6], train_ds)
-    y19 = encoded.numerical_feature(all_inputs[29], coords[1][7], train_ds)
-    y20 = encoded.numerical_feature(all_inputs[30], coords[1][8], train_ds)
-    y21 = encoded.numerical_feature(all_inputs[31], coords[1][9], train_ds)
-    y22 = encoded.numerical_feature(all_inputs[32], coords[1][10], train_ds)
-    y23 = encoded.numerical_feature(all_inputs[33], coords[1][11], train_ds)
-    y24 = encoded.numerical_feature(all_inputs[34], coords[1][12], train_ds)
-    y25 = encoded.numerical_feature(all_inputs[35], coords[1][13], train_ds)
-    y26 = encoded.numerical_feature(all_inputs[36], coords[1][14], train_ds)
-    y27 = encoded.numerical_feature(all_inputs[37], coords[1][15], train_ds)
-    y28 = encoded.numerical_feature(all_inputs[38], coords[1][16], train_ds)
-    y29 = encoded.numerical_feature(all_inputs[39], coords[1][17], train_ds)
-    y30 = encoded.numerical_feature(all_inputs[40], coords[1][18], train_ds)
-    y31 = encoded.numerical_feature(all_inputs[41], coords[1][19], train_ds)
-    y32 = encoded.numerical_feature(all_inputs[42], coords[1][20], train_ds)
-    y33 = encoded.numerical_feature(all_inputs[43], coords[1][21], train_ds)
+    y12 = encoded.numerical_feature(all_inputs[22], coords_p[1][0], train_ds)
+    y13 = encoded.numerical_feature(all_inputs[23], coords_p[1][1], train_ds)
+    y14 = encoded.numerical_feature(all_inputs[24], coords_p[1][2], train_ds)
+    y15 = encoded.numerical_feature(all_inputs[25], coords_p[1][3], train_ds)
+    y16 = encoded.numerical_feature(all_inputs[26], coords_p[1][4], train_ds)
+    y17 = encoded.numerical_feature(all_inputs[27], coords_p[1][5], train_ds)
+    y18 = encoded.numerical_feature(all_inputs[28], coords_p[1][6], train_ds)
+    y19 = encoded.numerical_feature(all_inputs[29], coords_p[1][7], train_ds)
+    y20 = encoded.numerical_feature(all_inputs[30], coords_p[1][8], train_ds)
+    y21 = encoded.numerical_feature(all_inputs[31], coords_p[1][9], train_ds)
+    y22 = encoded.numerical_feature(all_inputs[32], coords_p[1][10], train_ds)
+    y23 = encoded.numerical_feature(all_inputs[33], coords_p[1][11], train_ds)
+    y24 = encoded.numerical_feature(all_inputs[34], coords_p[1][12], train_ds)
+    y25 = encoded.numerical_feature(all_inputs[35], coords_p[1][13], train_ds)
+    y26 = encoded.numerical_feature(all_inputs[36], coords_p[1][14], train_ds)
+    y27 = encoded.numerical_feature(all_inputs[37], coords_p[1][15], train_ds)
+    y28 = encoded.numerical_feature(all_inputs[38], coords_p[1][16], train_ds)
+    y29 = encoded.numerical_feature(all_inputs[39], coords_p[1][17], train_ds)
+    y30 = encoded.numerical_feature(all_inputs[40], coords_p[1][18], train_ds)
+    y31 = encoded.numerical_feature(all_inputs[41], coords_p[1][19], train_ds)
+    y32 = encoded.numerical_feature(all_inputs[42], coords_p[1][20], train_ds)
+    y33 = encoded.numerical_feature(all_inputs[43], coords_p[1][21], train_ds)
 
-    z12 = encoded.numerical_feature(all_inputs[44], coords[2][0], train_ds)
-    z13 = encoded.numerical_feature(all_inputs[45], coords[2][1], train_ds)
-    z14 = encoded.numerical_feature(all_inputs[46], coords[2][2], train_ds)
-    z15 = encoded.numerical_feature(all_inputs[47], coords[2][3], train_ds)
-    z16 = encoded.numerical_feature(all_inputs[48], coords[2][4], train_ds)
-    z17 = encoded.numerical_feature(all_inputs[49], coords[2][5], train_ds)
-    z18 = encoded.numerical_feature(all_inputs[50], coords[2][6], train_ds)
-    z19 = encoded.numerical_feature(all_inputs[51], coords[2][7], train_ds)
-    z20 = encoded.numerical_feature(all_inputs[52], coords[2][8], train_ds)
-    z21 = encoded.numerical_feature(all_inputs[53], coords[2][9], train_ds)
-    z22 = encoded.numerical_feature(all_inputs[54], coords[2][10], train_ds)
-    z23 = encoded.numerical_feature(all_inputs[55], coords[2][11], train_ds)
-    z24 = encoded.numerical_feature(all_inputs[56], coords[2][12], train_ds)
-    z25 = encoded.numerical_feature(all_inputs[57], coords[2][13], train_ds)
-    z26 = encoded.numerical_feature(all_inputs[58], coords[2][14], train_ds)
-    z27 = encoded.numerical_feature(all_inputs[59], coords[2][15], train_ds)
-    z28 = encoded.numerical_feature(all_inputs[60], coords[2][16], train_ds)
-    z29 = encoded.numerical_feature(all_inputs[61], coords[2][17], train_ds)
-    z30 = encoded.numerical_feature(all_inputs[62], coords[2][18], train_ds)
-    z31 = encoded.numerical_feature(all_inputs[63], coords[2][19], train_ds)
-    z32 = encoded.numerical_feature(all_inputs[64], coords[2][20], train_ds)
-    z33 = encoded.numerical_feature(all_inputs[65], coords[2][21], train_ds)
+    z12 = encoded.numerical_feature(all_inputs[44], coords_p[2][0], train_ds)
+    z13 = encoded.numerical_feature(all_inputs[45], coords_p[2][1], train_ds)
+    z14 = encoded.numerical_feature(all_inputs[46], coords_p[2][2], train_ds)
+    z15 = encoded.numerical_feature(all_inputs[47], coords_p[2][3], train_ds)
+    z16 = encoded.numerical_feature(all_inputs[48], coords_p[2][4], train_ds)
+    z17 = encoded.numerical_feature(all_inputs[49], coords_p[2][5], train_ds)
+    z18 = encoded.numerical_feature(all_inputs[50], coords_p[2][6], train_ds)
+    z19 = encoded.numerical_feature(all_inputs[51], coords_p[2][7], train_ds)
+    z20 = encoded.numerical_feature(all_inputs[52], coords_p[2][8], train_ds)
+    z21 = encoded.numerical_feature(all_inputs[53], coords_p[2][9], train_ds)
+    z22 = encoded.numerical_feature(all_inputs[54], coords_p[2][10], train_ds)
+    z23 = encoded.numerical_feature(all_inputs[55], coords_p[2][11], train_ds)
+    z24 = encoded.numerical_feature(all_inputs[56], coords_p[2][12], train_ds)
+    z25 = encoded.numerical_feature(all_inputs[57], coords_p[2][13], train_ds)
+    z26 = encoded.numerical_feature(all_inputs[58], coords_p[2][14], train_ds)
+    z27 = encoded.numerical_feature(all_inputs[59], coords_p[2][15], train_ds)
+    z28 = encoded.numerical_feature(all_inputs[60], coords_p[2][16], train_ds)
+    z29 = encoded.numerical_feature(all_inputs[61], coords_p[2][17], train_ds)
+    z30 = encoded.numerical_feature(all_inputs[62], coords_p[2][18], train_ds)
+    z31 = encoded.numerical_feature(all_inputs[63], coords_p[2][19], train_ds)
+    z32 = encoded.numerical_feature(all_inputs[64], coords_p[2][20], train_ds)
+    z33 = encoded.numerical_feature(all_inputs[65], coords_p[2][21], train_ds)
 
-    v12 = encoded.numerical_feature(all_inputs[66], coords[3][0], train_ds)
-    v13 = encoded.numerical_feature(all_inputs[67], coords[3][1], train_ds)
-    v14 = encoded.numerical_feature(all_inputs[68], coords[3][2], train_ds)
-    v15 = encoded.numerical_feature(all_inputs[69], coords[3][3], train_ds)
-    v16 = encoded.numerical_feature(all_inputs[70], coords[3][4], train_ds)
-    v17 = encoded.numerical_feature(all_inputs[71], coords[3][5], train_ds)
-    v18 = encoded.numerical_feature(all_inputs[72], coords[3][6], train_ds)
-    v19 = encoded.numerical_feature(all_inputs[73], coords[3][7], train_ds)
-    v20 = encoded.numerical_feature(all_inputs[74], coords[3][8], train_ds)
-    v21 = encoded.numerical_feature(all_inputs[75], coords[3][9], train_ds)
-    v22 = encoded.numerical_feature(all_inputs[76], coords[3][10], train_ds)
-    v23 = encoded.numerical_feature(all_inputs[77], coords[3][11], train_ds)
-    v24 = encoded.numerical_feature(all_inputs[78], coords[3][12], train_ds)
-    v25 = encoded.numerical_feature(all_inputs[79], coords[3][13], train_ds)
-    v26 = encoded.numerical_feature(all_inputs[80], coords[3][14], train_ds)
-    v27 = encoded.numerical_feature(all_inputs[81], coords[3][15], train_ds)
-    v28 = encoded.numerical_feature(all_inputs[82], coords[3][16], train_ds)
-    v29 = encoded.numerical_feature(all_inputs[83], coords[3][17], train_ds)
-    v30 = encoded.numerical_feature(all_inputs[84], coords[3][18], train_ds)
-    v31 = encoded.numerical_feature(all_inputs[85], coords[3][19], train_ds)
-    v32 = encoded.numerical_feature(all_inputs[86], coords[3][20], train_ds)
-    v33 = encoded.numerical_feature(all_inputs[87], coords[3][21], train_ds)
+    # v12 = encoded.numerical_feature(all_inputs[66], coords_p[3][0], train_ds)
+    # v13 = encoded.numerical_feature(all_inputs[67], coords_p[3][1], train_ds)
+    # v14 = encoded.numerical_feature(all_inputs[68], coords_p[3][2], train_ds)
+    # v15 = encoded.numerical_feature(all_inputs[69], coords_p[3][3], train_ds)
+    # v16 = encoded.numerical_feature(all_inputs[70], coords_p[3][4], train_ds)
+    # v17 = encoded.numerical_feature(all_inputs[71], coords_p[3][5], train_ds)
+    # v18 = encoded.numerical_feature(all_inputs[72], coords_p[3][6], train_ds)
+    # v19 = encoded.numerical_feature(all_inputs[73], coords_p[3][7], train_ds)
+    # v20 = encoded.numerical_feature(all_inputs[74], coords_p[3][8], train_ds)
+    # v21 = encoded.numerical_feature(all_inputs[75], coords_p[3][9], train_ds)
+    # v22 = encoded.numerical_feature(all_inputs[76], coords_p[3][10], train_ds)
+    # v23 = encoded.numerical_feature(all_inputs[77], coords_p[3][11], train_ds)
+    # v24 = encoded.numerical_feature(all_inputs[78], coords_p[3][12], train_ds)
+    # v25 = encoded.numerical_feature(all_inputs[79], coords_p[3][13], train_ds)
+    # v26 = encoded.numerical_feature(all_inputs[80], coords_p[3][14], train_ds)
+    # v27 = encoded.numerical_feature(all_inputs[81], coords_p[3][15], train_ds)
+    # v28 = encoded.numerical_feature(all_inputs[82], coords_p[3][16], train_ds)
+    # v29 = encoded.numerical_feature(all_inputs[83], coords_p[3][17], train_ds)
+    # v30 = encoded.numerical_feature(all_inputs[84], coords_p[3][18], train_ds)
+    # v31 = encoded.numerical_feature(all_inputs[85], coords_p[3][19], train_ds)
+    # v32 = encoded.numerical_feature(all_inputs[86], coords_p[3][20], train_ds)
+    # v33 = encoded.numerical_feature(all_inputs[87], coords_p[3][21], train_ds)
 
     all_features = layers.concatenate(
         [
             x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, 
             x23, x24, x25, x26, x27, x28, x29, x30, x31, x32, x33,
 
-            y12, y13, y14, y15, y16, y17, y18, y19, y20, y21, y22, 
+            y12, y13, y14, y15, y16, y17, y18, y19, y20, y21, y22,
             y23, y24, y25, y26, y27, y28, y29, y30, y31, y32, y33,
 
             z12, z13, z14, z15, z16, z17, z18, z19, z20, z21, z22, 
             z23, z24, z25, z26, z27, z28, z29, z30, z31, z32, z33,
 
-            v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, 
-            v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33,
+            # v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22,
+            # v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33,
         ]
     )
     return all_features
@@ -346,9 +350,9 @@ if __name__ == '__main__':
     physical_devices = tf.config.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-    dataset_csv_file = './datasets/coords_dataset.csv'
+    dataset_csv_file = './datasets/numerical_coords_dataset.csv'
     target_value = 'class'
-    all_model = './model_weights/all_model/08.26/3_categories_pose' # all_model: Model struct and model weights.
+    all_model = './model_weights/all_model/08.27/3_categories_pose' # all_model: Model struct and model weights.
     
     # Data preprocessed and creat datasets.
     pose_datasets = CsvDataset(file=dataset_csv_file)
@@ -367,13 +371,13 @@ if __name__ == '__main__':
     all_inputs = input_features()
     all_features = specify_encoded_features(train_ds, all_inputs)
     x = layers.Dense(32, activation='relu')(all_features)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(0.3)(x)
     output = layers.Dense(1, activation='sigmoid')(x)
     model = keras.Model(all_inputs, output)
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # Model train.
-    model.fit(x=train_ds, epochs=50, verbose=1, validation_data=val_ds)
+    model.fit(x=train_ds, epochs=100, verbose=2, validation_data=val_ds)
 
     # Model save.
     model.save(all_model)
